d94d238473a9306db26fcd867383df902bc445e6
Takahiro <tkhr_621@docomo.ne.jp>
2023-06-03 23:29:53 +0900

    ユーザ作成ページを追加

---
+++ pages/user/create.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+  const { errorMessages, validate} = useUserValidation()
+  const name = ref('')
+  const email = ref('')
+  const submit = () => {
+    validate({
+      name: name.value,
+      email: email.value
+    })
+  }
+  
+</script>
+
+<template>
+  <div>
+    <h1>ユーザ登録</h1>
+    <v-sheet class="ma-4">
+      <v-form @submit.prevent="submit">
+        <v-row>
+          <v-col cols="2">
+            <v-list-subheader>
+              ユーザ名
+            </v-list-subheader>
+          </v-col>
+          <v-col cols="10">
+            <v-text-field
+              v-model="name"
+              label="ユーザ名"
+            >
+            </v-text-field>
+            <template
+              v-if="errorMessages && errorMessages.flatten().fieldErrors.name?.length"
+            >
+              <p
+                class="text-red-lighten-1 mb-4"
+                v-for="(error, i) in errorMessages.flatten().fieldErrors.name"
+                :key="i"
+              >
+                {{ error }}
+              </p>
+            </template>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="2">
+            <v-list-subheader>
+              メールアドレス
+            </v-list-subheader>
+          </v-col>
+          <v-col cols="10">
+            <v-text-field
+              v-model="email"
+              label="メールアドレス"
+              type="email"
+            >
+            </v-text-field>
+            <template
+              v-if="errorMessages && errorMessages.flatten().fieldErrors.email?.length"
+            >
+              <p
+                class="text-red-lighten-1 mb-4"
+                v-for="(error, i) in errorMessages.flatten().fieldErrors.email"
+                :key="i"
+              >
+                {{ error }}
+              </p>
+            </template>
+          </v-col>
+        </v-row>
+
+        <v-btn color="light-blue-accent-4" type="submit" class="mt-4 w-20">Submit</v-btn>
+      </v-form>
+    </v-sheet>
+  </div>
+</template>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,49 +1,10 @@
-<script setup lang="ts">
-  const submit = () => {
-    console.log('submit')
-  }
-  const name = ref('')
-  const email = ref('')
-</script>
-
 <template>
   <div>
-    <h1>ユーザ登録</h1>
+    <h1>ユーザ一覧</h1>
     <v-sheet class="ma-4">
-      <v-form @submit.prevent="submit">
-        <v-row>
-          <v-col cols="2">
-            <v-list-subheader>
-              ユーザ名
-            </v-list-subheader>
-          </v-col>
-          <v-col cols="10">
-            <v-text-field
-              v-model="name"
-              label="ユーザ名"
-            >
-            </v-text-field>
-          </v-col>
-        </v-row>
-
-        <v-row>
-          <v-col cols="2">
-            <v-list-subheader>
-              メールアドレス
-            </v-list-subheader>
-          </v-col>
-          <v-col cols="10">
-            <v-text-field
-              v-model="email"
-              label="メールアドレス"
-              type="email"
-            >
-            </v-text-field>
-          </v-col>
-        </v-row>
-
-        <v-btn color="light-blue-accent-4" type="submit" class="mt-4 w-20">Submit</v-btn>
-      </v-form>
+      <v-btn color="light-blue-accent-4" class="mt-4 w-20" to="/user/create">
+        ユーザ作成
+      </v-btn>
     </v-sheet>
   </div>
 </template>
