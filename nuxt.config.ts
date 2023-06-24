2f014c0791553b9d7f1d04d3668b92a5eb089276
Takahiro <tkhr_621@docomo.ne.jp>
2023-06-24 19:51:44 +0900

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+    typeCheck: true,
+    shim: false
+  },
   ssr: true,
   css: ['vuetify/styles'],
   build: {
